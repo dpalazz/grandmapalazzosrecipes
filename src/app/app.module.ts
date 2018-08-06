4a1d588b3c0421a12c3bf0c844c500429094c9bc
Dana Palazzo <dana@fretless.com>
2018-08-06 13:56:28 -0400

    Add recipe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeListComponent,
+    RecipeComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.css']
+})
+export class RecipeComponent implements OnInit {
+  @Input() recipe: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
